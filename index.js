9a3accda0fade5338f68e6d47629f04b729facc9
Guille-Rubio <guillermorubiog@gmail.com>
2022-09-03 11:18:55 +0200

    FIX require add in index

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express()
 const port = 3000
+const {add} = require('./utils/calculator');
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
